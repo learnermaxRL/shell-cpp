bb9f3e7dd9cc5c43801ee708c17a39377fb9e528
Mayank Sharma <sharma.mayank2125@gmail.com>
2025-03-03 17:24:58 +0530

    #cz2

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+
+std::string check_invalid(std::string& input) {
+
+    std::string err ;
+    err =  input +" "+":command not found" ;
+    return err;
+
+}
+
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
@@ -7,9 +17,19 @@ int main() {
 
   while (true)
   // Uncomment this block to pass the first stage
-  {  std::cout << "$ ";
-  std::string input;
-  std::getline(std::cin, input);
-  std::cout << std::endl;
+  {  
+    std::cout << "$ ";
+    std::string input;
+
+    std::getline(std::cin, input);
+
+    std::string err = check_invalid(input);
+
+    if (!err.empty())
+      {
+        std::cout << err ;
+        std::cout << std::endl;
+      }
+
   }
 }
