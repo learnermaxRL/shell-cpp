cc0b83895a3d62f91595b95d421d981d75cc8849
Mayank Sharma <sharma.mayank2125@gmail.com>
2025-03-03 16:56:10 +0530

    #008

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,11 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
+  while (true)
   // Uncomment this block to pass the first stage
-  std::cout << "$ ";
-
+  {  std::cout << "$ ";
   std::string input;
   std::getline(std::cin, input);
+  std::cout << std::endl;
+  }
 }
